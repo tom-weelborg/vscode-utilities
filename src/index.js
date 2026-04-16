d9367d7548bd995c0b011f79cc6a5efe0dbf54ba
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-16 22:39:03 +0200

    feat: add program

---
+++ src/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+	.name('vscode-utilities')
+	.description('CLI to some Visual Studio Code utilities')
+	.version('1.0.0')
+	.option('-c, --codium');
+
+program.parse();
