163285f5c713386fa3d38bfc9dc77463510a8822
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-18 19:11:45 +0200

    feat: add extension mapping features to merging of extension files

--- src/cli/merge-extensions.js
+++ src/cli/merge-extensions.js
@@ -1,3 +1,6 @@
+const { Option } = require('commander');
+
+const { mapToId, mapToNix } = require('../shared/extensions');
 const { getExtensions, readFiles, writeToFileRelative } = require('../shared/file');
 
 module.exports = (program) => {
@@ -7,8 +10,20 @@ module.exports = (program) => {
         .description('allows merging of extension files')
         .option('-s, --system', 'whether to use the system\'s `extensions.json` file')
         .option('-f, --files <paths...>', 'paths to extension files to merge')
+        .addOption(
+            new Option(
+                '-i, --id',
+                'reduces the list of extensions to a list of their ids'
+            ).conflicts('nix')
+        )
+        .addOption(
+            new Option(
+                '-n, --nix',
+                'maps the list of extensions to a format usable by `vscode-utils.extensionsFromVscodeMarketplace` in nix'
+            ).conflicts('id')
+        )
         .option('-e, --export-file <path>', 'exports the extensions to a file at the given relative path')
-        .action(async ({ system, files, exportFile }) => {
+        .action(async ({ system, files, id, nix, exportFile }) => {
             const { codium } = program.opts();
 
             let systemExtensions = [];
@@ -20,12 +35,18 @@ module.exports = (program) => {
                 f => JSON.parse(f)
             );
 
-            const extensions = [
+            let extensions = [
                 ...new Set([
                     ...systemExtensions,
                     ...filesExtensions,
                 ].map(e => JSON.stringify(e)))
             ].sort().map(e => JSON.parse(e));
+            
+            if (id) {
+                extensions = mapToId(extensions);
+            } else if (nix) {
+                extensions = await mapToNix(extensions);
+            }
 
             if (exportFile) {
                 writeToFileRelative(exportFile, JSON.stringify(extensions, null, 4));
